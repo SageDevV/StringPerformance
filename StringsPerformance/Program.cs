14107ce73aede06ae2a619b2079065e9d95c94b8
SageDevV <pandredbz@gmail.com>
2022-04-11 06:16:16 -0300

    first commtit

---
+++ StringsPerformance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+public class Program
+{
+    private const int REPETITIONS = 1000;
+
+    public static void StringTest()
+    {
+        string result = String.Empty;
+        for (int i = 0; i < REPETITIONS; i++)
+        {
+            result += "#";
+        }
+    }
+    public static void StringBuilderTest()
+    {
+        StringBuilder result = new(REPETITIONS);
+        for (int i = 0; i < REPETITIONS; i++)
+        {
+            result.Append("#");
+        }
+    }
+    static void Main(String[] args)
+    {
+        //StringTest();
+        StringBuilderTest();
+
+        Console.WriteLine("Press enter to exit...");
+        Console.Read();
+    }
+}
